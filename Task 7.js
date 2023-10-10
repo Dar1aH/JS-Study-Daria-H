faf123524b8b53817e804a368c8271dc206de10f
User <serjiii90@gmail.com>
2023-10-10 19:37:32 +0100

    Task 7 functions

--- Task 7.js
+++ Task 7.js
@@ -2,35 +2,30 @@
 За допомогою switch реалізуйте обчислення виразу, передаючи у switch, як параметр, символ математичної операції.
     Математичні операції для обчислення: "+", "-", "*", "/".
     Результат виводити в консоль.
+
 */
 
-let a = 23; // Change the numbers here
-let b = 5;  // to see different outputs 
 
-let result;
+let a = 10; 
+let b = 5; 
 
-switch('+'){
-    case '+':
-        result = a + b;
-        console.log(result);
-    break;    
+const sum = (a,b) =>{
+    return a + b
 }
-switch('-'){
-    case ('-'):
-        result = a - b;
-        console.log(result);
-    break;        
-}
-switch('*'){
-    case '*':
-        result = a * b;
-        console.log(result);
-    break;
+console.log(`${sum(a,b)}`);
 
+const subtract = (a,b) =>{
+    return a - b
 }
-switch('/'){
-    case '/':
-        result = a / b;
-        console.log(result);
-    break;    
-}        
+console.log(`${subtract(a,b)}`);
+
+const multiply = (a,b) =>{
+    return a * b
+}
+console.log(`${multiply(a,b)}`);
+
+const divide = (a,b) =>{
+    return a / b
+}
+console.log(`${divide(a,b)}`);
+
