9f5db610b97ed834bd796c600efaceb86508c377
User <serjiii90@gmail.com>
2023-10-10 19:11:02 +0100

    Task 6 functions

--- Task 6.js
+++ Task 6.js
@@ -4,29 +4,18 @@
     Результат виводити в консоль.
 */
 
-let DayOfTheWeek = 1;
-switch(true){
-    case DayOfTheWeek == 1:
-        console.log("Oh no, it's Monday again");
-        break;
-    case DayOfTheWeek == 2:
-        console.log("Today is Tuesday:( I wish it was Friday!");
-        break;
-    case DayOfTheWeek == 3:
-        console.log("It's Wednesday, we're half way through");
-        break;
-    case DayOfTheWeek == 4:
-        console.log("It's Thursday! We're almost there!");
-        break;
-    case DayOfTheWeek == 5:
-        console.log("It's Friday again:) Let's party!");
-        break;
-    case DayOfTheWeek == 6:
-        console.log("It's Saturday! Let's relax!");
-        break;
-    case DayOfTheWeek == 7:
-        console.log("It's Sunday! Let's get ready for the week");
-        break;
-    default:
-        console.log(DayOfTheWeek, "is the wrong number")                            
-}    
+
+let DayOfTheWeek = 5;
+
+const checkDayOfTheWeek = DayOfTheWeek =>{
+    console.log(DayOfTheWeek == 1 ? "Oh no, it's Monday again" :
+    DayOfTheWeek == 2 ? "Today is Tuesday:( I wish it was Friday!" :
+    DayOfTheWeek == 3 ? "It's Wednesday, we're half way through" :
+    DayOfTheWeek == 4 ? "It's Thursday! We're almost there!" :
+    DayOfTheWeek == 5 ? "It's Friday again:) Let's party!" :
+    DayOfTheWeek == 6 ? "It's Saturday! Let's relax!" :
+    DayOfTheWeek == 7 ? "It's Sunday! Let's get ready for the week:(" :
+    "Wrong input value");
+}
+checkDayOfTheWeek(DayOfTheWeek);
+
