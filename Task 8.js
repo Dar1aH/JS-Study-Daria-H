2fef28a3a134dcb9dd7fbd910050a46cf2fda4fe
User <serjiii90@gmail.com>
2023-10-10 19:46:58 +0100

    Task 8 functions

--- Task 8.js
+++ Task 8.js
@@ -1,34 +1,10 @@
 /*
 Використовуючи цикл for перемістити значення від 1 до 5 у масив myArray.
-
-
-let numbers = [1, 2, 3, 4, 5];
-for(let number of numbers){
-    console.log(number);
-}
-
-// Also possible like this
-
-let numbers = [1, 2, 3, 4, 5];
-for(let i=0; i<numbers.length; i+=1){
-    console.log(numbers[i]);
-}
-
-
-
-let myArray = [1, 2, 3, 4, 5];
-for(let number of myArray){
-    console.log(number);
-}
 */
 
-let array = []; 
+let myArray = [];
 
-let num1 = 1;
-let num2 = 2;
-let num3 = 3;
-let num4 = 4;
-let num5 = 5; 
+const pushToMyArray = (... numbers) => myArray.push(... numbers);
 
-array.push(num1, num2, num3, num4, num5);
-console.log(array);
+pushToMyArray(1, 2, 3, 4, 5);
+console.log(myArray);
