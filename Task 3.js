dd859c74933c144ddbf446a2cd6870491c84a29a
User <serjiii90@gmail.com>
2023-10-10 17:39:44 +0100

    Task 3 functions

--- Task 3.js
+++ Task 3.js
@@ -2,8 +2,9 @@
 Напишіть конструкцію if, щоб перевіряє, чи значення ціни товару не знаходиться між 1000 та 1900 включно.
     Реалізуйте два варіанти, один з оператором НЕ !, а інший без цього оператора.
     Результат виводити в консоль.
-*/
 
+
+// a) 
 
 let price = "Kim Kardashian"; 
 
@@ -13,6 +14,22 @@ const checkPrice = price => {
     return
     }
 console.log(!(price>=1000 && price<=1900) ? "This is valid input" : "This is invalid input");    
-};
+}
 
 checkPrice(price);
+*/
+
+//b)
+
+let price = "Patrick";
+
+const checkPrice = price => {
+    if(typeof price == 'string'){
+        console.log(`${price} is not a number`);
+        return
+    }
+console.log(price<1000 || price>1900 ? "This is valid input" :
+"This is invalid input");     
+}
+
+checkPrice(price); 
