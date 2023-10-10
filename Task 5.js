c68584fba9c58f8cc0545664a9af6e9429663e4f
User <serjiii90@gmail.com>
2023-10-10 18:51:25 +0100

    Task 5 functions

--- Task 5.js
+++ Task 5.js
@@ -4,59 +4,16 @@
     (не середнє арифметичне значення, а яке з трьох заданих чисел среднє за значенням) використовуючи оператор if-else.
     Використати вкладені оператори if
     Результат виводити в консоль.
-
-
-let a = 23; // Change the numbers (a, b, c) here :) 
-let b = 45; // No, I didn't use Chat GPT:) 
-let c = 19; // Yes, I wrote this myself! Awesome, right:) 
-a !== b;
-b !== c;
-c !== a; 
-printNumber(a,b,c);
-function printNumber(a,b,c){
-    if((a>b && a<c) || (a>c && a<b)){
-        console.log(a);
-    }
-    else if((b>a && b<c) || (b>c && b<a)){
-        console.log(b);
-    }
-    else if((c>a && c<b) || (c>b && c<a)){
-        console.log(c);
-    }
-    else{
-        console.log("Invalid input");
-    }
-}
 */
-let a = 10, b = 50, c =30;
-let avg;
-if (a > b){
-    if(a < c){
-        avg = a;
-    } 
-else{
-    if (b > c){
-        avg = b;
-    }
-else{
-       avg = c;
-} 
- } 
-  }
-else {
-    if(b < c){
-        avg = b;
-    }
-    else{
-        if(a > c){
-            avg = a;
-        }
-    else{
-        avg = c;
-    }
-  }
+
+
+let a = 80, b = 10, c =30;
+
+const median = (a, b, c) =>{
+    console.log( (a>b && a<c) || (a>c && a<b) ? `${a} is the median of the numbers` :
+               (b>a && b<c) || (b>c && b<a) ? `${b} is the median of the numbers`:
+               (c>a && c<b) || (c>b && c<a) ? `${c} is the median of the numbers` :
+               "Invalid input");
 }
-console.log(avg); 
-
-
+median(a, b, c); 
 
